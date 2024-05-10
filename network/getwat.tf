cbea6bb5983185dd03d83980bb189115dc10e3ea
Heba Adel <85371523+heba120@users.noreply.github.com>
2024-05-11 01:21:41 +0300

    Update getwat.tf

--- network/getwat.tf
+++ network/getwat.tf
@@ -5,3 +5,15 @@ resource "aws_internet_gateway" "gw" {
     Name = "${var.commenname}main_gw"
   }
 }
+
+resource "aws_eip" "nat-eip" {
+  domain = "vpc"
+}
+resource "aws_nat_gateway" "example" {
+  allocation_id = aws_eip.nat-eip.id
+  subnet_id     = aws_subnet.public1.id
+
+  tags = {
+    Name = "gw NAT"
+  }
+}
